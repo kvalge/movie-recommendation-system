12a0f0e667d7cbaad044ca8022a034defd2a2592
katri <kvalge@taltech.ee>
2025-04-29 20:09:19 +0300

    Updated.

--- BLL/obj/Debug/net9.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net9.0/BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83690cf885deb6c5295e829ab727ffe73426e14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2dc2dd8750dfa14ad0e9b626c3a2096c450b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
